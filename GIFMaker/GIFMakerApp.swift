89cac81f58dd5ed8dce58766cd7e629f4d080cca
Josh Kowarsky <josh.kowarsky@gmail.com>
2020-12-12 01:26:50 -0800

    Initial Commit

---
+++ GIFMaker/GIFMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIFMakerApp.swift
+//  GIFMaker
+//
+//  Created by Josh Kowarsky on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct GIFMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
